57e877275b5c9d7fe6ac76110520b7e206c69f85
Sup3rone <48138402+Sup3rone@users.noreply.github.com>
2019-10-10 07:47:40 -0500

    Add files via upload

--- util/base.sql
+++ util/base.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2
--- http://www.phpmyadmin.net
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 30-12-2012 a las 14:35:23
--- Versión del servidor: 5.5.16-log
--- Versión de PHP: 5.3.8
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-10-2019 a las 14:46:25
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.2.22
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `base`
 --
-CREATE DATABASE `base` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `base`;
 
 -- --------------------------------------------------------
 
@@ -28,12 +28,11 @@ USE `base`;
 -- Estructura de tabla para la tabla `historial`
 --
 
-CREATE TABLE IF NOT EXISTS `historial` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `historial` (
+  `id` int(11) NOT NULL,
   `evento` varchar(250) NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=46 ;
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `historial`
@@ -84,7 +83,18 @@ INSERT INTO `historial` (`id`, `evento`, `fecha`) VALUES
 (42, 'Se insertó un nuevo usuario con ID: 39 con el nombre: Alfredo', '2012-11-14 16:47:27'),
 (43, 'Se insertó un nuevo usuario con ID: 10 con el nombre: Alfredo', '2012-11-14 18:35:27'),
 (44, 'Se insertó un nuevo usuario con ID: 10 con el nombre: Ángel', '2012-11-23 20:45:34'),
-(45, 'Se insertó un nuevo usuario con ID: 12 con el nombre: Ricardo', '2012-11-23 21:50:08');
+(45, 'Se insertó un nuevo usuario con ID: 12 con el nombre: Ricardo', '2012-11-23 21:50:08'),
+(46, 'Se insertó un nuevo usuario con ID: 13 con el nombre: Emmanuel', '2019-10-03 12:55:59'),
+(47, 'Se insertó un nuevo usuario con ID: 16 con el nombre: Emmanuel', '2019-10-03 12:59:51'),
+(48, 'Se insertó un nuevo usuario con ID: 17 con el nombre: Emanuel', '2019-10-10 12:38:58'),
+(49, 'Se insertó un nuevo usuario con ID: 18 con el nombre: Ford', '2019-10-10 12:39:55'),
+(50, 'Se insertó un nuevo usuario con ID: 19 con el nombre: Firefox', '2019-10-10 12:40:28'),
+(51, 'Se insertó un nuevo usuario con ID: 20 con el nombre: google', '2019-10-10 12:40:38'),
+(52, 'Se insertó un nuevo usuario con ID: 21 con el nombre: lenovo', '2019-10-10 12:41:48'),
+(53, 'Se insertó un nuevo usuario con ID: 22 con el nombre: Legion', '2019-10-10 12:43:31'),
+(54, 'Se insertó un nuevo usuario con ID: 23 con el nombre: Comodo300', '2019-10-10 12:44:21'),
+(55, 'Se insertó un nuevo usuario con ID: 24 con el nombre: Harman', '2019-10-10 12:45:08'),
+(56, 'Se insertó un nuevo usuario con ID: 25 con el nombre: Aula3f', '2019-10-10 12:45:34');
 
 -- --------------------------------------------------------
 
@@ -92,14 +102,12 @@ INSERT INTO `historial` (`id`, `evento`, `fecha`) VALUES
 -- Estructura de tabla para la tabla `puestos`
 --
 
-CREATE TABLE IF NOT EXISTS `puestos` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `puestos` (
+  `id` bigint(20) NOT NULL,
   `puesto` varchar(250) NOT NULL,
   `depende` bigint(20) NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `depende` (`depende`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `puestos`
@@ -121,14 +129,12 @@ INSERT INTO `puestos` (`id`, `puesto`, `depende`, `fecha`) VALUES
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuarios` (
+  `id` bigint(20) NOT NULL,
   `nombre` varchar(200) NOT NULL,
   `contraseña` char(32) NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `nombre` (`nombre`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -138,19 +144,23 @@ INSERT INTO `usuarios` (`id`, `nombre`, `contraseña`, `fecha`) VALUES
 (1, 'Galileo', 'd41d8cd98f00b204e9800998ecf8427e', '2011-02-26 06:00:00'),
 (2, 'Alicia', 'd41d8cd98f00b204e9800998ecf8427e', '2012-02-17 04:11:36'),
 (3, 'Rafael', 'd41d8cd98f00b204e9800998ecf8427e', '2012-02-17 04:12:03'),
-(8, 'Yesenia', 'f795e34c94384805f4e8da7d98effc81', '2012-05-13 03:57:52'),
-(9, 'Ma. Soledad', 'd41d8cd98f00b204e9800998ecf8427e', '2012-05-13 03:57:58'),
-(10, 'Ángel', '42332323', '2012-11-23 20:45:34'),
-(12, 'Ricardo', 'e10adc3949ba59abbe56e057f20f883e', '2012-11-23 21:50:08');
+(16, 'Emmanuel', '1234445', '2019-10-03 12:59:51'),
+(17, 'Emanuel', '1234mkvae', '2019-10-10 12:38:58'),
+(18, 'Ford', '12234mkvae', '2019-10-10 12:39:55'),
+(19, 'Firefox', '312de', '2019-10-10 12:40:28'),
+(20, 'google', '312de', '2019-10-10 12:40:38'),
+(21, 'lenovo', '312d21e', '2019-10-10 12:41:48'),
+(22, 'Legion', 'adsmo21', '2019-10-10 12:43:31'),
+(23, 'Comodo300', '3ing49', '2019-10-10 12:44:21'),
+(24, 'Harman', '3ing2d49', '2019-10-10 12:45:08'),
+(25, 'Aula3f', '3ingw22d49', '2019-10-10 12:45:34');
 
 --
 -- Disparadores `usuarios`
 --
-DROP TRIGGER IF EXISTS `nuevo_usuario`;
-DELIMITER //
-CREATE TRIGGER `nuevo_usuario` AFTER INSERT ON `usuarios`
- FOR EACH ROW INSERT INTO historial (evento) values ( concat('Se insertó un nuevo usuario con ID: ', NEW.id, ' con el nombre: ', NEW.nombre) )
-//
+DELIMITER $$
+CREATE TRIGGER `nuevo_usuario` AFTER INSERT ON `usuarios` FOR EACH ROW INSERT INTO historial (evento) values ( concat('Se insertó un nuevo usuario con ID: ', NEW.id, ' con el nombre: ', NEW.nombre) )
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -159,16 +169,13 @@ DELIMITER ;
 -- Estructura de tabla para la tabla `usuarios_puestos`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios_puestos` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuarios_puestos` (
+  `id` int(10) UNSIGNED NOT NULL,
   `usuario` bigint(11) NOT NULL,
   `puesto` bigint(11) NOT NULL,
-  `inicio` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `fin` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `usuario` (`usuario`),
-  KEY `puesto` (`puesto`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `inicio` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fin` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios_puestos`
@@ -178,6 +185,66 @@ INSERT INTO `usuarios_puestos` (`id`, `usuario`, `puesto`, `inicio`, `fin`) VALU
 (1, 1, 2, '2012-02-26 21:38:46', NULL),
 (4, 2, 3, '2012-02-26 22:23:31', NULL),
 (5, 3, 4, '2012-02-26 22:23:31', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `puestos`
+--
+ALTER TABLE `puestos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `depende` (`depende`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `usuarios_puestos`
+--
+ALTER TABLE `usuarios_puestos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `puesto` (`puesto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `historial`
+--
+ALTER TABLE `historial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT de la tabla `puestos`
+--
+ALTER TABLE `puestos`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_puestos`
+--
+ALTER TABLE `usuarios_puestos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
@@ -195,6 +262,7 @@ ALTER TABLE `puestos`
 ALTER TABLE `usuarios_puestos`
   ADD CONSTRAINT `usuarios_puestos_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `usuarios_puestos_ibfk_2` FOREIGN KEY (`puesto`) REFERENCES `puestos` (`id`) ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
